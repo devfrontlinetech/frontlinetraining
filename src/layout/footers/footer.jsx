d975a78dd63e32156e1ba59aa54cebe9c2b806b1
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-16 18:10:36 +0530

    Update footer.jsx

--- src/layout/footers/footer.jsx
+++ src/layout/footers/footer.jsx
@@ -51,7 +51,7 @@ const Footer = ({ style_2, dark_bg,home_4 }) => {
                             <div className="edu-footer-widget">
                                 <div className="logo">
                                     <Link href={'/'}>
-                                        <a>
+                                        <a  aria-label="Frontlinetraining">
                                             {!dark_bg && <>
                                                 {!style_2 && <img className="logo-light" src={logoLight} alt="Corporate Logo" />}
                                                 <img className="logo-dark" src={logoDark} alt="Corporate Logo" />
