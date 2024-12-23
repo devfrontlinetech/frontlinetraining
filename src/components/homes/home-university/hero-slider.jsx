9daef52757c1e9645f006cf1a534405241fcb9b0
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-23 13:23:32 +0530

    Update hero-slider.jsx

--- src/components/homes/home-university/hero-slider.jsx
+++ src/components/homes/home-university/hero-slider.jsx
@@ -77,7 +77,7 @@ const HeroSlider = () => {
                                                 </h3>
                                                 <p data-sal-delay="200" data-sal="slide-up" data-sal-duration="1000">{sm_text}</p>
                                                 <div className="banner-btn" data-sal-delay="400" data-sal="slide-up" data-sal-duration="1000">
-                                                    <Link href="/course-style-1">
+                                                    <Link href="/web-dev">
                                                     <a className="edu-btn btn-secondary">{btn_text} <i className="icon-4"></i></a>
                                                     </Link>
                                                 </div>
