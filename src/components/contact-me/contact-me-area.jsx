1dfcf6e2eb30be5ce5977745002920415d805dca
devfrontlinetech <frontlinetech47@gmail.com>
2024-12-23 11:05:37 +0530

    Update contact-me-area.jsx

--- src/components/contact-me/contact-me-area.jsx
+++ src/components/contact-me/contact-me-area.jsx
@@ -56,7 +56,7 @@ const ContactMeArea = () => {
                                         </li>
                                     </ul>
                                     <ul className="social-share">
-                                        <li><a href="#"><i className="icon-share-alt"></i></a></li>
+                                        <li><a href="https://www.whatsapp.com/"><i className="icon-share-alt"></i></a></li>
                                         <SocialShare/>
                                     </ul>
                                 </div>
