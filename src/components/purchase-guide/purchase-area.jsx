fd6f4c782925cae511554f2543f6835d579a5ccb
devfrontlinetech <frontlinetech47@gmail.com>
2025-01-23 11:39:47 +0530

    Update purchase-area.jsx

--- src/components/purchase-guide/purchase-area.jsx
+++ src/components/purchase-guide/purchase-area.jsx
@@ -40,7 +40,18 @@ const PurchaseArea = () => {
 
                             <div className="text-block">
                                 <h4 className="title">How to Purchase a Course?</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis aliquip commodo consequat aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+                                <p>Click on any course title to view more details, including course content, pricing, and instructor information.</p>
+                                <p>Once you’ve found a course you’re interested in, click on the "Enroll Now" button on the course page.</p>
+                                <p>You will be taken to a checkout page where you can review your selection.</p>
+                                <p>If you don’t already have an account, you’ll be prompted to Create an Account by entering your email and setting a password</p>
+                                <p>After logging in, you’ll be taken to the Shopping Cart where you can review your course selection.</p>
+                                <p>Click "Proceed to Checkout" to enter your payment information.</p>
+                                <p>You’ll be asked to provide your payment details (e.g., credit card or PayPal).</p>
+                                <p>Fill in the required information on the payment page. We accept a variety of payment methods (credit/debit cards, PayPal, etc.).</p>
+                                <p>After entering your payment info, click "Confirm Payment".</p>
+                                <p>Once your payment is processed, you’ll see a confirmation page that your order has been successfully completed.</p>
+                                <p>Simply go to your Account Dashboard, where you’ll find your enrolled courses.</p>
+                                <p>Click on the course title to start learning!</p>
                             </div>
 
                             <div className="text-block">
