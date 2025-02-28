e21593d03c11482f63601189818bfedcdcc41e88
sumesh <sumesh@frontlinetech.in>
2025-02-28 19:11:06 +0530

    Added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -22,7 +22,7 @@ function MyApp( { Component, pageProps } ) {
     }, [router.asPath] ); 
 
     useEffect( () => {        
-        sal();
+        sal();   
     }, [] );
     return (
         <React.Fragment>
