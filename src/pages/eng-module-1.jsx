3f8ccf9c5a97a3777123a5ecab585c2c00a8b3a5
sumesh <sumesh@frontlinetech.in>
2025-08-15 11:24:24 +0530

    Adding folders and files

---
+++ src/pages/eng-module-1.jsx
@@ -0,0 +1,28 @@
+import SEO from '../components/seo';
+import { Wrapper } from '../layout';
+import EnglishModuleMain from '../components/eng-modules';
+
+const EnglishModule = () => {
+    return (
+        <Wrapper>
+            <SEO pageTitle={'English Module'} />
+            <EnglishModuleMain />
+        </Wrapper>
+        
+    )
+}
+
+export default EnglishModule;
+
+
+
+
+
+
+
+
+
+
+
+
+
