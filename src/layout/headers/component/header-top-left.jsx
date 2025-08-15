f7f40afcb05e17a6c6040bd9eb63388bfad3d684
sumesh <sumesh@frontlinetech.in>
2025-08-15 11:53:27 +0530

    added files

--- src/layout/headers/component/header-top-left.jsx
+++ src/layout/headers/component/header-top-left.jsx
@@ -5,7 +5,7 @@ const HeaderTopLeft = () => {
         <div className="header-notify">
             First 20 students get 50% discount. <a href="contact-us">Hurry up!</a>
         </div>
-    )
+    )   
 }
 
 export default HeaderTopLeft;
