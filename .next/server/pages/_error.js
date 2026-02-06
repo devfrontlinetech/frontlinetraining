6ab6f52a6fa5d8bcbef8ac50ec31fbf878641099
sumesh <sumesh@frontlinetech.in>
2026-02-06 12:00:23 +0530

    remove .next build folder

--- .next/server/pages/_error.js
+++
@@ -1,9 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
-R.c("server/chunks/ssr/node_modules_next_bbf269ba._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__84687bd7._.js")
-R.c("server/chunks/ssr/[externals]_next_dist_compiled_@opentelemetry_api_2f2eda7e._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__4396b000._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__68d47023._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__8352c24f._.js")
-R.m(48461)
-module.exports=R.m(48461).exports
--- .next/server/pages/api/event.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/pages/api/event.js")
-R.c("server/chunks/[root-of-the-server]__d2ee4dd8._.js")
-R.c("server/chunks/[root-of-the-server]__ec476382._.js")
-R.m(26149)
-module.exports=R.m(26149).exports
