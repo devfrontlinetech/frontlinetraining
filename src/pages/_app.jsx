3a71ef313d759c81d3ad6806041290c40ca3f5c4
sumesh <sumesh@frontlinetech.in>
2026-02-06 11:13:39 +0530

    added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,101 +1,43 @@
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { Provider } from "react-redux";
-import sal from "sal.js";
-import { ThemeProvider } from "next-themes";
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import sal from 'sal.js';
+import { ThemeProvider } from 'next-themes';
+if (typeof window !== 'undefined') {
+  require( 'bootstrap/dist/js/bootstrap' );
+}
+import '../styles/index.scss';
+import { store } from '../redux/store';
+import Theme from '../components/common/theme';
+import { MouseMoveProvider } from '../contexts/mouse-move-context';
+import SEO from '../components/seo';
+import { DefaultSeo } from 'next-seo';
+import defaultSEOConfig from '../../next-seo.config';
 
-import "../styles/index.scss";
 
-/* Move plain CSS imports here (NOT in SCSS) */
-import "swiper/css/bundle";
-import "react-image-lightbox/style.css";
-import "react-toastify/dist/ReactToastify.css";
-import "../styles/vendor/icomoon.css";
-import "../styles/vendor/remixicon.css";
+function MyApp( { Component, pageProps } ) {
+    const router = useRouter();
+    useEffect( () => {
+        sal( { threshold: 0.1, once: true } );
+    }, [router.asPath] ); 
 
-
-import { store } from "../redux/store";
-import Theme from "../components/common/theme";
-import { MouseMoveProvider } from "../contexts/mouse-move-context";
-import SEO from "../components/seo";
-import { DefaultSeo } from "next-seo";
-import defaultSEOConfig from "../../next-seo.config";
-
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  /* Better Bootstrap JS loading for Next */
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
-
-  useEffect(() => {
-    sal({ threshold: 0.1, once: true });
-  }, [router.asPath]);
-
-  useEffect(() => {
-    sal();
-  }, []);
-
-  return (
-    <>
-      <SEO font="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Spartan:wght@400;500;600;700;800&display=swap" />
-      <Provider store={store}>
-        <ThemeProvider defaultTheme="light">
-          <MouseMoveProvider>
-            <DefaultSeo {...defaultSEOConfig} />
-            <Component {...pageProps} />
-          </MouseMoveProvider>
-          <Theme />
-        </ThemeProvider>
-      </Provider>
-    </>
-  );
+    useEffect( () => {        
+        sal();   
+    }, [] );
+    return (
+        <React.Fragment>
+            <SEO font="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Spartan:wght@400;500;600;700;800&display=swap" />
+            <Provider store={ store }>
+                <ThemeProvider defaultTheme="light">
+                    <MouseMoveProvider>
+                    <DefaultSeo {...defaultSEOConfig} />
+                        <Component { ...pageProps } />
+                    </MouseMoveProvider>
+                    <Theme />
+                </ThemeProvider>
+            </Provider>
+        </React.Fragment>
+    )
 }
 
 export default MyApp;
-
-// OLD FILE 05-02-2026
-
-// import { useRouter } from 'next/router';
-// import React, { useEffect } from 'react';
-// import { Provider } from 'react-redux';
-// import sal from 'sal.js';
-// import { ThemeProvider } from 'next-themes';
-// if (typeof window !== 'undefined') {
-//   require( 'bootstrap/dist/js/bootstrap' );
-// }
-// import '../styles/index.scss';
-// import { store } from '../redux/store';
-// import Theme from '../components/common/theme';
-// import { MouseMoveProvider } from '../contexts/mouse-move-context';
-// import SEO from '../components/seo';
-// import { DefaultSeo } from 'next-seo';
-// import defaultSEOConfig from '../../next-seo.config';
-
-// function MyApp( { Component, pageProps } ) {
-//     const router = useRouter();
-//     useEffect( () => {
-//         sal( { threshold: 0.1, once: true } );
-//     }, [router.asPath] );
-
-//     useEffect( () => {
-//         sal();
-//     }, [] );
-//     return (
-//         <React.Fragment>
-//             <SEO font="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Spartan:wght@400;500;600;700;800&display=swap" />
-//             <Provider store={ store }>
-//                 <ThemeProvider defaultTheme="light">
-//                     <MouseMoveProvider>
-//                     <DefaultSeo {...defaultSEOConfig} />
-//                         <Component { ...pageProps } />
-//                     </MouseMoveProvider>
-//                     <Theme />
-//                 </ThemeProvider>
-//             </Provider>
-//         </React.Fragment>
-//     )
-// }
-
-// export default MyApp;
