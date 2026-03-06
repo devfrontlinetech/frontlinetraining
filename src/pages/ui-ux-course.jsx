e30a245187b7b1dba2b66fb8f11965ed0c369ab9
sumesh <sumesh@frontlinetech.in>
2026-03-06 15:21:42 +0530

    uiux page added

---
+++ src/pages/ui-ux-course.jsx
@@ -0,0 +1,14 @@
+import SEO from '../components/seo';
+import { Wrapper } from '../layout';
+import CourseStyleSixMain from '../components/course-style-6';
+
+const UiuxCourse = () => {
+    return (
+        <Wrapper>
+            <SEO pageTitle={'UI/UX Course'} />
+            <CourseStyleSixMain />
+        </Wrapper>
+    )
+}
+
+export default UiuxCourse;
