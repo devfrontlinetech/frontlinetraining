fab42ebb08a80be5038ff15cb9210bcd34941a76
sumesh <sumesh@frontlinetech.in>
2026-03-06 15:14:23 +0530

    updated files

--- src/data/index.js
+++ src/data/index.js
@@ -3,9 +3,11 @@ import instructors_data from "./instructors";
 import blog_data from "./blog-data";
 import event_data from "./event-data";
 import shop_data from "./shop-data";
+import course_two_data from "./course-2-data";
 
 
 export {
+  course_two_data,
   course_data,
   instructors_data,
   blog_data,
