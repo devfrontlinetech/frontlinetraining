22c89c7d065076115ef098ca1201d75111b93c57
sumesh <sumesh@frontlinetech.in>
2026-03-07 10:55:11 +0530

    added

--- src/pages/course-details/index.jsx
+++ src/pages/course-details/index.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import SEO from '../../components/seo';
-import { Wrapper } from '../../layout';
-import CourseDetailsMain from '../../components/course-details';
-import { course_data } from '../../data';
+import { useRouter } from "next/router";
+import React from "react";
+import SEO from "../../components/seo";
+import { Wrapper } from "../../layout";
+import CourseDetailsMain from "../../components/course-details";
 
-const course = course_data[0];
+import { course_data } from "../../data/course-data";
+import { course_two_data } from "../../data/course-2-data";
 
 const CourseDetails = () => {
-    return (
-        <Wrapper>
-            <SEO pageTitle={'Course Details'} />
-            <CourseDetailsMain course={course} />
-        </Wrapper>
-    )
-}
+  const router = useRouter();
+  const { id } = router.query;
+
+  const allCourses = [...course_data, ...course_two_data];
+
+  const course = allCourses.find((item) => String(item.id) === String(id));
+
+  if (!course) return <div>Loading...</div>;
+
+  return (
+    <Wrapper>
+      <SEO pageTitle={course.title} />
+      <CourseDetailsMain course={course} />
+    </Wrapper>
+  );
+};
 
 export default CourseDetails;
+
+
+
+// import React from 'react';
+// import SEO from '../../components/seo';
+// import { Wrapper } from '../../layout';
+// import CourseDetailsMain from '../../components/course-details';
+// import { course_data } from '../../data';
+
+// const course = course_data[0];
+
+// const CourseDetails = () => {
+//     return (
+//         <Wrapper>
+//             <SEO pageTitle={'Course Details'} />
+//             <CourseDetailsMain course={course} />
+//         </Wrapper>
+//     )
+// }
+
+// export default CourseDetails;
