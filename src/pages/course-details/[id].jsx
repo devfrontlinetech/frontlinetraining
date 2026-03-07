432212e64bb04ef9006138e153a40359101ffa7d
sumesh <sumesh@frontlinetech.in>
2026-03-07 10:51:13 +0530

    added

--- src/pages/course-details/[id].jsx
+++ src/pages/course-details/[id].jsx
@@ -1,40 +1,103 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import SEO from '../../components/seo';
-import { course_data } from '../../data';
-import { Wrapper } from '../../layout';
-import CourseDetailsMain from '../../components/course-details';
+import { useRouter } from "next/router";
+import React from "react";
+import SEO from "../../components/seo";
+import { Wrapper } from "../../layout";
+import CourseDetailsMain from "../../components/course-details";
+
+import { course_data } from "../../data/course-data";
+import { course_two_data } from "../../data/course-2-data";
 
 const DynamicCourseDetails = () => {
-    const router = useRouter();
-    const { id } = router.query;
-    const course = course_data.find(item => String(item.id) === String(id))
-    return (
-        <Wrapper>
-            <SEO pageTitle={'Course Details'} />
-            <CourseDetailsMain course={course} />
-        </Wrapper>
-    )
-}
+  const router = useRouter();
+  const { id } = router.query;
+
+ 
+  const allCourses = [...course_data, ...course_two_data];
+
+  const course = allCourses.find(
+    (item) => String(item.id) === String(id)
+  );
+
+  if (!course) return <div>Loading...</div>;
+
+  return (
+    <Wrapper>
+      <SEO pageTitle={course.title} />
+      <CourseDetailsMain course={course} />
+    </Wrapper>
+  );
+};
 
 export default DynamicCourseDetails;
 
-export async function getStaticPaths() {
-    const paths = course_data.map((course) => {
-        return {
-            params:{
-                id:`${course.id}`
-            }
-        }
-    })
-    return {
-      paths,
-      fallback: false,
-    }
-  }
 
-export async function getStaticProps(context) {
-    return {
-        props: {}
-    }
+export async function getStaticPaths() {
+
+  const allCourses = [...course_data, ...course_two_data];
+
+  const paths = allCourses.map((course) => ({
+    params: {
+      id: String(course.id),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
+
+
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
+
+
+
+
+
+
+
+
+// import { useRouter } from 'next/router';
+// import React from 'react';
+// import SEO from '../../components/seo';
+// import { course_data } from '../../data';
+// import { Wrapper } from '../../layout';
+// import CourseDetailsMain from '../../components/course-details';
+
+// const DynamicCourseDetails = () => {
+//     const router = useRouter();
+//     const { id } = router.query;
+//     const course = course_data.find(item => String(item.id) === String(id))
+//     return (
+//         <Wrapper>
+//             <SEO pageTitle={'Course Details'} />
+//             <CourseDetailsMain course={course} />
+//         </Wrapper>
+//     )
+// }
+
+// export default DynamicCourseDetails;
+
+// export async function getStaticPaths() {
+//     const paths = course_data.map((course) => {
+//         return {
+//             params:{
+//                 id:`${course.id}`
+//             }
+//         }
+//     })
+//     return {
+//       paths,
+//       fallback: false,
+//     }
+//   }
+
+// export async function getStaticProps(context) {
+//     return {
+//         props: {}
+//     }
+// }
