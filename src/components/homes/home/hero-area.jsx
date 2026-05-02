0e3c28828a6a625427590d3d453f1f020dd318ef
sumesh <sumesh@frontlinetech.in>
2026-05-02 11:47:29 +0530

    modified

--- src/components/homes/home/hero-area.jsx
+++ src/components/homes/home/hero-area.jsx
@@ -14,7 +14,7 @@ const HeroArea = () => {
   const [selectedTime, setSelectedTime] = useState(null);
   // Dates for April and May 2026
   const availableDates = [
-    "2026-05-02",
+    
     "2026-05-03",
     "2026-05-05",
     "2026-05-10",
