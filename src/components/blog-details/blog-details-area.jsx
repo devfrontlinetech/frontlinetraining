41d9b9ec7782f416edeec6ea986c41295f3b2624
sumesh <sumesh@frontlinetech.in>
2026-05-06 17:17:51 +0530

    added

--- src/components/blog-details/blog-details-area.jsx
+++ src/components/blog-details/blog-details-area.jsx
@@ -1,107 +1,149 @@
-import React from 'react';
-import BlogSidebar from '../blog/blog-sidebar';
-import BlogCommentForm from '../forms/blog-comment-form';
-import CommentArea from './comment-area';
+import React from "react";
+import BlogSidebar from "../blog/blog-sidebar";
+import BlogCommentForm from "../forms/blog-comment-form";
+import CommentArea from "./comment-area";
 
-const BlogDetailsArea = ({blog}) => {
-    return (
-        <div className="blog-details-area section-gap-equal">
-            <div className="container">
-                <div className="row row--30">
-                    <div className="col-lg-8">
-                        <div className="blog-details-content">
-                            <div className="entry-content">
-                                <span className="category">Developer</span>
-                                <h3 className="title">{blog?.title}</h3>
-                                <ul className="blog-meta">
-                                    <li><i className="icon-27"></i>{blog?.date}</li>
-                                    <li><i className="icon-28"></i>Com {blog?.comment}</li>
-                                </ul>
-                                <div className="thumbnail">
-                                    <img src="/assets/images/blog/blog-large-1.jpg" alt="Blog Image" />
-                                </div>
-                            </div>
+const BlogDetailsArea = ({ blog }) => {
+  return (
+    <div className="blog-details-area section-gap-equal">
+      <div className="container">
+        <div className="row row--30">
+          <div className="col-lg-8">
+            <div className="blog-details-content">
+              <div className="entry-content">
+                <span className="category">Developer</span>
+                <h3 className="title">{blog?.title}</h3>
+                <ul className="blog-meta">
+                  <li>
+                    <i className="icon-27"></i>
+                    {blog?.date}
+                  </li>
+                  <li>
+                    <i className="icon-28"></i>Com {blog?.comment}
+                  </li>
+                </ul>
+                <div className="thumbnail">
+                  <img
+                    src="/assets/images/blog/blog-large-1.jpg"
+                    alt="Blog Image"
+                  />
+                </div>
+              </div>
 
-                            <p>Education shapes our lives by empowering us with knowledge and skills, paving the way for personal growth and societal progress. Through education, we have the power to change the future, fostering innovation, equality, and a better world for all. </p>
+              <p>
+                Education shapes our lives by empowering us with knowledge and
+                skills, paving the way for personal growth and societal
+                progress. Through education, we have the power to change the
+                future, fostering innovation, equality, and a better world for
+                all.{" "}
+              </p>
 
-                            <p>In this way, education has the transformative power to change the future for the better, creating a more informed, compassionate, and sustainable world for generations to come.</p>
-                            <ul>
-                                <li>Empower Through Knowledge, Shape Tomorrow’s World.</li>
-                                <li>Education is the Key to a Future Without Limits.</li>
-                                <li>Learn Today, Lead Tomorrow.</li>
-                            </ul>
+              <p>
+                In this way, education has the transformative power to change
+                the future for the better, creating a more informed,
+                compassionate, and sustainable world for generations to come.
+              </p>
+              <ul>
+                <li>Empower Through Knowledge, Shape Tomorrow’s World.</li>
+                <li>Education is the Key to a Future Without Limits.</li>
+                <li>Learn Today, Lead Tomorrow.</li>
+              </ul>
 
-{/*                             <blockquote>
-                                <p></p>
-                                <h5 className="author">Simon Baker</h5>
-                            </blockquote> */}
+              {/*   <blockquote>
+                        <p></p>
+                        <h5 className="author">Simon Baker</h5>
+                    </blockquote> */}
 
-                            <h3 className="title">Transforming Businesses with Technology</h3>
-                            <p> "Digitally Transforming Your Business for Success" Description: Focus on how your IT solutions help businesses enhance their operations, improve efficiency, and accelerate growth using the latest tech tools and platforms.</p>
+              <h3 className="title">Transforming Businesses with Technology</h3>
+              <p>
+                {" "}
+                "Digitally Transforming Your Business for Success" Description:
+                Focus on how your IT solutions help businesses enhance their
+                operations, improve efficiency, and accelerate growth using the
+                latest tech tools and platforms.
+              </p>
 
-                            <div className="features-image">
-                                <div className="row g-md-5">
-                                    <div className="col-6">
-                                        <div className="thumb">
-                                            <img src="/assets/images/blog/moti-1.jpg" alt="Features Images" />
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <div className="thumb">
-                                            <img src="/assets/images/blog/moti-2.jpg" alt="Features Images" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+              <div className="features-image">
+                <div className="row g-md-5">
+                  <div className="col-6">
+                    <div className="thumb">
+                      <img
+                        src="/assets/images/blog/moti-1.jpg"
+                        alt="Features Images"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="thumb">
+                      <img
+                        src="/assets/images/blog/moti-2.jpg"
+                        alt="Features Images"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-                            <p></p>
+              <p></p>
 
-                            <p>Our programs are designed to ignite your passion and empower you to make an impact.</p>
+              <p>
+                Our programs are designed to ignite your passion and empower you
+                to make an impact.
+              </p>
 
-                            <h3 className="title">Dream Big, Learn Bigger</h3>
-                            <p>In the world of IT, there are no limits to what you can achieve. Dream big, learn bigger, and let your journey towards greatness begin with us.</p>
+              <h3 className="title">Dream Big, Learn Bigger</h3>
+              <p>
+                In the world of IT, there are no limits to what you can achieve.
+                Dream big, learn bigger, and let your journey towards greatness
+                begin with us.
+              </p>
 
-                            <ul>
-                                <li>Focus on Soft Skills</li>
-                                <li>Never Stop Learning</li>
-                                <li>Work on Real Projects</li>
-                                
-                            </ul>
+              <ul>
+                <li>Focus on Soft Skills</li>
+                <li>Never Stop Learning</li>
+                <li>Work on Real Projects</li>
+              </ul>
 
-                            <div className="blog-share-area">
-                                <div className="row align-items-center">
-                                    <div className="col-md-7">
-                                        <div className="blog-tags">
-                                            <h6 className="title">Tags:</h6>
-                                            <div className="tag-list">
-                                                <a href="contact-us">Language</a>
-                                                <a href="contact-us">eLearn</a>
-                                                <a href="contact-us">Tips</a>
-                                            </div>
-                                        </div>
-                                    </div>
+              <div className="blog-share-area">
+                <div className="row align-items-center">
+                  <div className="col-md-7">
+                    <div className="blog-tags">
+                      <h6 className="title">Tags:</h6>
+                      <div className="tag-list">
+                        <a href="contact-us">Language</a>
+                        <a href="contact-us">eLearn</a>
+                        <a href="contact-us">Tips</a>
+                      </div>
+                    </div>
+                  </div>
 
-                                    <div className="col-md-5">
-                                        <div className="blog-share">
-                                            <h6 className="title">Share on:</h6>
-                                            <ul className="social-share icon-transparent">
-                                                <li>
-                                                    <a href=" https://www.facebook.com/frontlinetechin/"><i className="icon-facebook"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://twitter.com/FrontlineTechin"><i className="icon-twitter"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.instagram.com/frontlinetechnologies_/"><i className="icon-instagram"></i></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                  <div className="col-md-5">
+                    <div className="blog-share">
+                      <h6 className="title">Share on:</h6>
+                      <ul className="social-share icon-transparent">
+                        <li>
+                          <a href=" https://www.facebook.com/frontlinetechin/">
+                            <i className="icon-facebook"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://twitter.com/FrontlineTechin">
+                            <i className="icon-twitter"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.instagram.com/frontlinetechnologies_/">
+                            <i className="icon-instagram"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-{/*                         <div className="blog-author">
+            {/*                         <div className="blog-author">
                             <div className="thumbnail">
                                 <img src="/assets/images/blog/author-01.jpg" alt="Author Images" />
                             </div>
@@ -144,26 +186,26 @@ const BlogDetailsArea = ({blog}) => {
                             </div>
                         </div> */}
 
-                        {/*  Start Comment Area  */}
-{/*                         <CommentArea /> */}
-                        {/*  End Comment Area  */}
-                        <div className="comment-form-area">
-                            <h3 className="heading-title">Leave Your Comment Here</h3>
-                            {/* form start */}
-                            <BlogCommentForm />
-                            {/* form end */}
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4">
-                        {/* sidebar start */}
-                        <BlogSidebar />
-                        {/* sidebar end */}
-                    </div>
-                </div>
+            {/*  Start Comment Area  */}
+            {/*                         <CommentArea /> */}
+            {/*  End Comment Area  */}
+            <div className="comment-form-area">
+              <h3 className="heading-title">Leave Your Comment Here</h3>
+              {/* form start */}
+              <BlogCommentForm />
+              {/* form end */}
             </div>
+          </div>
+
+          <div className="col-lg-4">
+            {/* sidebar start */}
+            <BlogSidebar />
+            {/* sidebar end */}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default BlogDetailsArea;
