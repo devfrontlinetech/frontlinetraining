4c7e058a6b1cbf525790522d3037534a365807d6
sumesh <sumesh@frontlinetech.in>
2026-05-06 16:51:01 +0530

    added

--- src/components/homes/home/hero-area.jsx
+++ src/components/homes/home/hero-area.jsx
@@ -15,7 +15,7 @@ const HeroArea = () => {
   // Dates for April and May 2026
   const availableDates = [
   
-    "2026-05-05",
+    "2026-05-06",
     "2026-05-10",
     "2026-05-20",
     "2026-05-21",
