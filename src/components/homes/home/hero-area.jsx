42294f9db0ad39597e6d7c823c9b74f25ea83685
sumesh <sumesh@frontlinetech.in>
2026-08-22 16:16:18 +0530

    added

--- src/components/homes/home/hero-area.jsx
+++ src/components/homes/home/hero-area.jsx
@@ -169,7 +169,7 @@ const HeroArea = () => {
                     className="title"
                     data-sal-delay="100"
                     data-sal="slide-up"
-                    data-sal-duration="1000"
+                    data-sal-duration="1000" 
                   >
                     <span className="color-secondary">IT</span> Training in
                     karur
@@ -183,7 +183,7 @@ const HeroArea = () => {
                     State of the Art IT training center in karur. We impart it
                     and software development courses with industry exposure and
                     job assistance
-                  </p>
+                  </p> 
 
                   <div
                     className="banner-btn"
